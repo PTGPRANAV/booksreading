737069355371d081c0853689bcdc7afb91ac421c
PTGPRANAV <pranavdaundkar13@gmail.com>
2022-01-05 01:14:08 +0530

    books reading app

---
+++ app/src/main/java/eu/ptgpranav/booksreading/scifi.kt
@@ -0,0 +1,11 @@
+package eu.ptgpranav.booksreading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class scifi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scifi)
+    }
+}
---
+++ app/src/main/java/eu/ptgpranav/booksreading/taketour1.kt
@@ -0,0 +1,11 @@
+package eu.ptgpranav.booksreading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class taketour1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taketour1)
+    }
+}
